8fc41d89192541114572c1effdc6ba4576bcc2e6
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2012-09-05 15:32:38 -0500

    Refactoring the config logic to use hte new authn config api

--- lib/authn-activation.rb
+++ lib/authn-activation.rb
@@ -1,3 +1,9 @@
 require 'authn'
 require_relative 'authn/activation/version'
 require_relative 'authn/model'
+
+module AuthN
+  module Activation
+
+  end
+end
--- lib/authn/activation.rb
+++
@@ -1,9 +0,0 @@
-module AuthN
-  module Activation
-    autoload :Config, "authn/activation/config"
-
-    def self.config
-      @config ||= Config.new Config::DEFAULTS
-    end
-  end
-end
