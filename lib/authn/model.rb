055734d4e0d1154b2188006727a36a40ff3de921
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2012-09-05 15:33:41 -0500

    Requiring in the activation model logic

--- lib/authn/model.rb
+++ lib/authn/model.rb
@@ -1,3 +1,5 @@
+require_relative 'activation/model'
+
 module AuthN
   module Model
     module ClassMethods
---
+++ test/lib/authn/activation/model_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'helper'
+
+class TestAuthNActivationModel < MiniTest::Unit::TestCase
+  def test_that_things_work
+    assert true
+  end
+end
