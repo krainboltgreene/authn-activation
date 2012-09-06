eed05c55338c97ba5d1247d98299ec20b9783a09
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-09-05 23:38:19 -0500

    FIxing a bug with the method find call for activation mail

--- test/lib/authn/activation/version_test.rb
+++ test/lib/authn/activation/version_test.rb
@@ -3,6 +3,6 @@ require 'helper'
 
 class TestAuthNActivationVersion < MiniTest::Unit::TestCase
   def test_that_version_is_latest
-    assert_equal "1.8.1", AuthN::Activation::VERSION
+    assert_equal "1.8.2", AuthN::Activation::VERSION
   end
 end
